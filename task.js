bed45479c78a1cd3d4b31b4fe47a5e51eb946e00
yiranzi <wuchuck@wudeMacBook-Pro.local>
2017-02-05 21:25:03 -0600

    can runver1.0

--- task.js
+++ task.js
@@ -14,69 +14,78 @@
  *    "北京": 90,
  *    "上海": 40
  * };
+ * 1使用标记ID的方式..
+ *
  */
 var aqiData = new Array();
+aqiData.iNumber = 0;
 
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
-function pt(a) {
-    console.log(a);
-}
+
 function addAqiData() {
+
     var oCityName = document.getElementById("aqi-city-input");
     var sCityName = oCityName.value;
-    pt(sCityName);
     var oData = document.getElementById("aqi-value-input");
     var nData = oData.value;
-    pt(nData);
-    aqiData.push([sCityName,nData])
+    //检测数据合理性
+    aqiData.push([aqiData.iNumber,sCityName,nData])
+    aqiData.iNumber++;
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+    if(aqiData.length ==1 )
+    {
+        renderHead();
+    }
     var nodeTr = null;
     var nodeFather = null;
-    if(aqiData.length == 1)
+    nodeTr = document.createElement("tr");
+    nodeTr.iNumber = aqiData[aqiData.length-1][0];
+    nodeFather = document.getElementById("aqi-table");
+    nodeFather = nodeFather.appendChild(nodeTr);
+    for(var i = 0;i<3;i++)
     {
-        nodeTr = document.createElement("tr");
-
-        nodeFather = document.getElementById("aqi-table");
-        nodeFather = nodeFather.appendChild(nodeTr);
-        for(var i = 0;i<3;i++)
-        {
-            nodeTr = document.createElement("td");
-            nodeFather.appendChild(nodeTr);
-        }
-        nodeFather.children[0].innerHTML = "ID";
-        nodeFather.children[1].innerHTML = "姓名";
-        nodeFather.children[2].innerHTML = "年龄";
+        var node = document.createElement("td");
+        nodeFather.appendChild(node);
     }
-    else
+    nodeFather.children[0].innerHTML = aqiData[aqiData.length-1][1];
+    nodeFather.children[1].innerHTML = aqiData[aqiData.length-1][2];
+    var oBtn = document.createElement("button");
+    nodeFather.children[2].appendChild(oBtn);
+    oBtn.innerHTML = "删除";
+    oBtn.onclick = delBtnHandle;
+    console.log(nodeTr.iNumber);
+}
+
+function renderDelete() {
+    //处理删除最后一个元素
+    //遍历查找到那个元素.
+    //删除那个节点
+}
+
+function renderHead()
+{
+    var nodeTr = null;
+    var nodeFather = null;
+    nodeTr = document.createElement("tr");
+    nodeTr.iNumber = aqiData[aqiData.length-1][0];
+    nodeFather = document.getElementById("aqi-table");
+    nodeFather = nodeFather.appendChild(nodeTr);
+    for(var i = 0;i<3;i++)
     {
-        nodeTr = document.createElement("tr");
-        nodeTr.iIndex = aqiData.length;
-        nodeFather = document.getElementById("aqi-table");
-        nodeFather = nodeFather.appendChild(nodeTr);
-        for(var i = 0;i<3;i++)
-        {
-            nodeTr = document.createElement("td");
-            nodeFather.appendChild(nodeTr);
-        }
-        nodeFather.children[0].innerHTML = aqiData[aqiData.length-1][1];
-        nodeFather.children[1].innerHTML = aqiData[aqiData.length-1][2];
-       // node
-        //nodeFather.children[2].appendChild()
-         //   .innerHTML = <button>删除</button>;
+        var node = document.createElement("td");
+        nodeFather.appendChild(node);
     }
-    console.log(nodeTr.iIndex);
-
-
-
-
+    nodeFather.children[0].innerHTML = "ID";
+    nodeFather.children[1].innerHTML = "姓名";
+    nodeFather.children[2].innerHTML = "年龄";
 }
 
 /**
@@ -94,8 +103,20 @@ function addBtnHandle() {
  */
 function delBtnHandle() {
     // do sth.
+    var oTr = this.parentNode.parentNode;
+    var index = oTr.iNumber;
+    console.log("删除了第几个"+index);
+    for(var i=0;i<aqiData.length;i++)
+    {
+        if(aqiData[i][0] == index)
+        {
+            console.log(aqiData.splice(i,1));
+            break;
+        }
+    }
+    oTr.parentNode.removeChild(oTr);
 
-    renderAqiList();
+    //renderDelete(oTr);
 }
 
 function init() {
@@ -105,7 +126,6 @@ function init() {
 
 
     var oBtn = document.getElementById("add-btn");
-    pt(oBtn);
     oBtn.onclick = addBtnHandle;
 }
 
